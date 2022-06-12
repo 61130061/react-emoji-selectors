0681d9b8f3b8c3a1704bab62a35169cd0a0747c1
61130061 <thezeroyou@gmail.com>
2022-06-12 22:41:14 +0700

    Duplicate function in Core.js

--- src/r-emoji-selector/EmojiSelector.js
+++ src/r-emoji-selector/EmojiSelector.js
@@ -3,26 +3,10 @@ import './emoji.css';
 
 import EmojiGroup from './data/emoji-group-data';
 import EmojiList from './data/emoji-list-data';
+import { useOnClickOutside } from './Core';
 
 import Input from './components/Input';
 import Footer from './components/Footer';
-
-function useOnClickOutside (active, ref, callback) {
-   useEffect(() => {
-      function handleClickOutside(event) {
-         if (ref.current && !ref.current.contains(event.target) && active) {
-               callback();
-         }
-      }
-      // Bind the event listener
-      document.addEventListener("mousedown", handleClickOutside);
-      return () => {
-         // Unbind the event listener on clean up
-         document.removeEventListener("mousedown", handleClickOutside);
-      };
-   }, [ref]);
-}
-
 
 function EmojiSelector ({ 
    onClose, 
--- src/r-emoji-selector/EmojiSelectorMobile.js
+++ src/r-emoji-selector/EmojiSelectorMobile.js
@@ -2,24 +2,9 @@ import React, { useRef, useState, useEffect } from 'react';
 
 import EmojiGroup from './data/emoji-group-data';
 import EmojiList from './data/emoji-list-data';
+import { useOnClickOutside } from './Core';
 
 import Category from './components/Category';
-
-function useOnClickOutside (active, ref, callback) {
-   useEffect(() => {
-      function handleClickOutside(event) {
-         if (ref.current && !ref.current.contains(event.target) && active) {
-               callback();
-         }
-      }
-      // Bind the event listener
-      document.addEventListener("mousedown", handleClickOutside);
-      return () => {
-         // Unbind the event listener on clean up
-         document.removeEventListener("mousedown", handleClickOutside);
-      };
-   }, [ref]);
-}
 
 function EmojiSelectorMobile ({ 
    onClose, 
